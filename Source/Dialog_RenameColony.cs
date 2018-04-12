a8c86e6c799f65fa6c104b7340cabb0ad1ef87a7
NeonSpec <xcellghoul@gmail.com>
2018-04-13 01:06:58 +0200

    Add project files.

---
+++ Source/Dialog_RenameColony.cs
@@ -0,0 +1,57 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using Verse;
+
+namespace RenameColony
+{
+    class Dialog_RenameColony : Dialog_GiveName
+    {
+        private FactionBase factionBase;
+
+        public Dialog_RenameColony(FactionBase factionBase)
+        {
+            this.factionBase = factionBase;
+            if (factionBase.HasMap && factionBase.Map.mapPawns.FreeColonistsSpawnedCount != 0)
+            {
+                Pawn suggestingPawn = factionBase.Map.mapPawns.FreeColonistsSpawned.RandomElement<Pawn>();
+            }
+            this.curName = Faction.OfPlayer.Name;
+            this.nameMessageKey = "RCNamePlayerFactionMessage";
+            this.invalidNameMessageKey = "PlayerFactionNameIsInvalid";
+            this.useSecondName = true;
+            this.curSecondName = factionBase.Name;
+            this.secondNameMessageKey = "RCNamePlayerFactionBaseMessage_NameFactionContinuation";
+            this.invalidSecondNameMessageKey = "RCPlayerFactionBaseNameIsInvalid";
+            this.gainedNameMessageKey = "RCPlayerFactionAndBaseGainsName";
+        }
+
+        public override void PostOpen()
+        {
+            base.PostOpen();
+            if (this.factionBase.Map != null)
+            {
+                Current.Game.VisibleMap = this.factionBase.Map;
+            }
+        }
+
+        protected override bool IsValidName(string s)
+        {
+            return NamePlayerFactionDialogUtility.IsValidName(s);
+        }
+
+        protected override bool IsValidSecondName(string s)
+        {
+            return NamePlayerFactionBaseDialogUtility.IsValidName(s);
+        }
+
+        protected override void Named(string s)
+        {
+            NamePlayerFactionDialogUtility.Named(s);
+        }
+
+        protected override void NamedSecond(string s)
+        {
+            NamePlayerFactionBaseDialogUtility.Named(this.factionBase, s);
+        }
+    }
+}
---
+++ Source/RenameColony.cs
@@ -0,0 +1,43 @@
+﻿using Harmony;
+using RimWorld;
+using RimWorld.Planet;
+using System.Reflection;
+using UnityEngine;
+using Verse;
+
+namespace RenameColony
+{
+    public class Mod : Verse.Mod
+    {
+        public Mod(ModContentPack content) : base(content)
+        {
+            HarmonyInstance rchi = HarmonyInstance.Create("neon.rimworld.renamecolony");
+            MethodInfo targetMethoderino = AccessTools.Method(typeof(PlaySettings), "DoPlaySettingsGlobalControls");
+            HarmonyMethod postFixerino = new HarmonyMethod(typeof(RenameColony).GetMethod("AddWidget"));
+            rchi.Patch(targetMethoderino, null, postFixerino);
+            Log.Message("[RenameColony] Postfixed");
+        }
+
+        public class RenameColony
+        {
+            public static void AddWidget(WidgetRow row, bool worldView)
+            {
+                if (worldView)
+                {
+                    #if DEBUG
+                    Log.Message("femboy foxes are gay");
+                    #endif
+                    return;
+                }
+                if (row.ButtonIcon(ContentFinder<Texture2D>.Get("Rename"), "Rename the Colony!"))
+                {
+                    FactionBase factionBase = (FactionBase)Find.VisibleMap.info.parent;
+                    Find.WindowStack.Add(new Dialog_RenameColony(factionBase));
+                    #if DEBUG
+                    Log.Message("femboy foxes are gay");
+                    #endif
+                }
+            }
+        }
+    }
+}
